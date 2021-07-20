8307e7b10e330f5006e5f3073a461a8aff0d2272
RitikBansal07 <80676724+RitikBansal07@users.noreply.github.com>
2021-07-20 14:20:30 +0530

    Add files via upload

---
+++ Sample_port_Scanner.py
@@ -0,0 +1,39 @@
+#!/bin/python3 #Generally used in kali. (#! is shebang.)
+#Made by Ritik Bansal
+# boiler plate of Sample Port Scanner
+#It isn't perfect. We can't get everything in very beginning. Its a bad port scanner.
+
+import sys
+import socket
+from datetime import datetime
+
+#Define our target
+if
+        target =  #Translates hostname to IPv4
+else:
+        print("Invalid amount of arguments.")
+        print("Syntax: python3 scanner.py <ip>")
+
+#Add a pretty banner - Just for fun!!!
+print("-" * 70)
+print("Scanning target "+target)
+print("Time started: "+str(datetime.now()))
+print("-" * 70)
+
+try:
+          for port in range(1,65535): #Depends on how much range you want to put in, if you know which port is open in that ip address... cause its so, lengthy and time taking.
+
+
+                                                        #It returns an error indicator.
+                  print("Checking port {}".format(port))   #It is optional depends on you whether wanna write it or not. Cause this line of code will umm... not gonna break suspense, run it see for yourself. Its fun!!!
+                  if result == 0:
+                          print("Port {} is open".format(port))
+
+
+except  #KeyboardInterrupt
+
+
+except  #hostname could not be resolved
+
+
+except  #couldn't connect to server
